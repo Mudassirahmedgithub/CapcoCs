045cc5ba6562d4bdf79bb9011355997228f1787e
Mudassirahmedgithub <120584423+Mudassirahmedgithub@users.noreply.github.com>
2025-11-26 07:12:54 +0530

    jlasdjflsdjhmn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,10 +6,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function getUserEmail(user: UserResource | User | null) {
+export function getUserEmail(user: any) {
   const email =
-    user?.emailAddresses?.find((e) => e.id === user.primaryEmailAddressId)
+    user?.emailAddresses?.find((e: any) => e.id === user?.primaryEmailAddressId)
       ?.emailAddress ?? "";
 
   return email;
 }
+
